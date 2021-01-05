86193c7d8a81a4e9980c6124133fe6f585ce6ca6
Phineas09 <claudiu.ghenea15@yahoo.ro>
2021-01-05 17:41:50 +0200

    :wrench: Added JSON suport and prepare requests
    Am adaugat JSON si am pregatit aplicatia pentru request-uri catre
    weatherthemap
    
    Signed-off-by: Phineas09 <claudiu.ghenea15@yahoo.ro>

--- TitanStorm/src/main/java/module-info.java
+++ TitanStorm/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module ro.mta.se.lab {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
-    opens ro.mta.se.lab to javafx.fxml;
     opens ro.mta.se.lab.controller to javafx.fxml;
     exports ro.mta.se.lab;
 }
--- TitanStorm/src/main/java/ro/mta/se/lab/controller/PrimaryController.java
+++ TitanStorm/src/main/java/ro/mta/se/lab/controller/PrimaryController.java
@@ -2,6 +2,7 @@ package ro.mta.se.lab.controller;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import org.json.JSONObject;
 import ro.mta.se.lab.Main;
 
 public class PrimaryController {
@@ -9,5 +10,6 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         Main.setRoot("secondary");
+        JSONObject test = new JSONObject();
     }
 }
